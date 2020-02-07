77734366c25d8b668e489df28ea58031c598427d
Derrick Mugwaneza <DerrickM@magenic.com>
2020-02-07 16:47:55 -0600

    Updating EmployeeController methods

--- src/main/java/com/magenic/jmaqs/webservices/integration/EmployeeController.java
+++ src/main/java/com/magenic/jmaqs/webservices/integration/EmployeeController.java
@@ -2,9 +2,13 @@ package com.magenic.jmaqs.webservices.integration;
 
 import com.magenic.jmaqs.webservices.BaseWebServiceTest;
 import com.magenic.jmaqs.webservices.*;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import sun.awt.windows.WEmbeddedFrame;
 
+import javax.swing.text.StringContent;
 import java.net.*;
 
 public class EmployeeController {
@@ -17,8 +21,29 @@ public class EmployeeController {
   }
 
   public CloseableHttpResponse getSpecificEmployee(WebServiceDriver webServiceDriver, String iD) throws Exception{
-    CloseableHttpResponse result = webServiceDriver.getContent("$/api/EmployeesAPI/GetEmployee/{iD}",
+    CloseableHttpResponse result = webServiceDriver.getContent(String.format("/api/EmployeesAPI/PutEmployee/{0}", iD),
         ContentType.APPLICATION_JSON,true);
+
+    return result;
+  }
+
+  public CloseableHttpResponse updateSpecificEmployee(WebServiceDriver webServiceDriver, HttpEntity content, String iD) throws Exception{
+    CloseableHttpResponse result = webServiceDriver.putContent(String.format("/api/EmployeesAPI/PutEmployee/{0}", iD), content,
+        ContentType.APPLICATION_JSON, true);
+
+    return result;
+  }
+
+  public CloseableHttpResponse addEmployee(WebServiceDriver webServiceDriver, HttpEntity content) throws Exception{
+    CloseableHttpResponse result = webServiceDriver.postContent("/api/EmployeesAPI/PostEmployee", content,
+        ContentType.APPLICATION_JSON, true);
+
+    return result;
+  }
+
+  public CloseableHttpResponse removeEmployee(WebServiceDriver webServiceDriver, String iD) throws Exception{
+    CloseableHttpResponse result = webServiceDriver.deleteContent(String.format("/api/EmployeesAPI/DeleteEmployee/{0}", iD),
+        ContentType.APPLICATION_JSON, false);
 
     return result;
   }
